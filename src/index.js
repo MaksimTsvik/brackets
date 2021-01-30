14b6d1a55c4080e6b55f0b487eb67e5fc018dcdb
MaksimTsvik <mkdemix@tut.by>
2021-01-30 17:15:01 +0300

    Updated str length inspection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   // Check string length (if !%2=0 -> false)
-  if (!str.length % 2) {
+  if (!(str.length % 2)) {
     return false;
   }
+
 }
