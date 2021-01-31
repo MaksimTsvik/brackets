5699ac02f4c9594f2aa9b45fc26256b519d00f62
MaksimTsvik <mkdemix@tut.by>
2021-01-31 13:17:10 +0300

    Added solution plan

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@ module.exports = function check(str, bracketsConfig) {
   if (!(str.length % 2)) {
     return false;
   }
+  // Create an 'bracket' array or other counter
+  // Let's inspect if each elem in str is equal to 1st elem (0) in each config element
+  // if yes - add into 'bracket' array
+  // if no check with 2nd elem (1) in each config element and with last elem in 'counter'
+  // if yes - remove 1 last elem from 'bracket' array
+  // if no - false
+  let bracket = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if 
+    }
+  }
 
+
+
+
+
+  return true;
 }
