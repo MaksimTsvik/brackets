49bbee539a1ed10d3282f13e441322d354fbfc07
MaksimTsvik <mkdemix@tut.by>
2021-01-31 14:43:41 +0300

    Added solution, 3 test failed.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // Check string length (if !%2=0 -> false)
-  if (!(str.length % 2)) {
+  // Check string length (if str.length % 2!=0 -> false)
+  if (str.length % 2) {
     return false;
   }
-  // Create an 'bracket' array or other counter
+  // Create an 'bracket' array to store each unclosed bracket
   // Let's inspect if each elem in str is equal to 1st elem (0) in each config element
   // if yes - add into 'bracket' array
   // if no check with 2nd elem (1) in each config element and with last elem in 'counter'
   // if yes - remove 1 last elem from 'bracket' array
-  // if no - false
+  // if no - continue to inspect
   let bracket = [];
+
   for (let i = 0; i < str.length; i++) {
+
     for (let j = 0; j < bracketsConfig.length; j++) {
-      if 
+
+      if (str[i] == bracketsConfig[j][0]) {
+        bracket.push(str[i]);
+      }
+
+      if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] == bracket[bracket.length - 1]) {
+        bracket.pop();
+
+      }
+
     }
+
   }
 
-
-
-
+  if (bracket.length != 0) {
+    return false;
+  }
 
   return true;
 }
